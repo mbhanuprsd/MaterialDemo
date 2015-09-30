cc5cfce5cb025e8ce68ae4f563b88d3d3c36371f
Bhanu Prasad Merakanapalli <bhanuprasad.m@xcubelabs.com>
2015-09-30 13:22:05 +0530

    Started tab implementation

--- app/src/main/java/com/xcubelabs/bhanuprasadm/materialdemo/activity/MainActivity.java
+++ app/src/main/java/com/xcubelabs/bhanuprasadm/materialdemo/activity/MainActivity.java
@@ -2,17 +2,30 @@ package com.xcubelabs.bhanuprasadm.materialdemo.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.xcubelabs.bhanuprasadm.materialdemo.fragment.NavigationDrawerFragment;
 import com.xcubelabs.bhanuprasadm.materialdemo.R;
+import com.xcubelabs.bhanuprasadm.materialdemo.fragment.NavigationDrawerFragment;
+import com.xcubelabs.bhanuprasadm.materialdemo.tabs.SlidingTabLayout;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager mPager;
+    private SlidingTabLayout mTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,11 @@ public class MainActivity extends AppCompatActivity{
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        mTabs.setViewPager(mPager);
     }
 
     @Override
@@ -33,7 +51,7 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menuItem1:
                 Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_LONG).show();
                 break;
@@ -46,5 +64,56 @@ public class MainActivity extends AppCompatActivity{
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        String[] tabs;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabs = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            MyFragment myFragment = MyFragment.getInstance(position);
+            return myFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return tabs.length;
+        }
+    }
+
+    public static class MyFragment extends Fragment {
+
+        TextView tvPosition;
+
+        public static MyFragment getInstance(int position) {
+            MyFragment fragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.fragment_my, container, false);
+            tvPosition = (TextView) layout.findViewById(R.id.tvPosition);
+            Bundle bundle = getArguments();
+            if (bundle != null) {
+                tvPosition.setText("Selected fragment position : " + bundle.getInt("position", 0));
+            }
+            return layout;
+        }
     }
 }
