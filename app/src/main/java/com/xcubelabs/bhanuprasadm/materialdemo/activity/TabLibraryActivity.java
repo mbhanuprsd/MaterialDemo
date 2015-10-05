736e9754d2fbece428a4452fce1d8534825bef7b
Bhanu Prasad Merakanapalli <bhanuprasad.m@xcubelabs.com>
2015-10-05 11:55:23 +0530

    Lecture 28

--- app/src/main/java/com/xcubelabs/bhanuprasadm/materialdemo/activity/TabLibraryActivity.java
+++ app/src/main/java/com/xcubelabs/bhanuprasadm/materialdemo/activity/TabLibraryActivity.java
@@ -1,14 +1,26 @@
 package com.xcubelabs.bhanuprasadm.materialdemo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.xcubelabs.bhanuprasadm.materialdemo.R;
 
-public class TabLibraryActivity extends AppCompatActivity {
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+
+public class TabLibraryActivity extends AppCompatActivity implements MaterialTabListener {
+
+    private MaterialTabHost materialTabHost;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +28,28 @@ public class TabLibraryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tab_library);
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        //noinspection ConstantConditions
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        materialTabHost = (MaterialTabHost) findViewById(R.id.materialTabHost);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        //noinspection deprecation
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                materialTabHost.setSelectedNavigationItem(position);
+            }
+        });
+
+        for (int i = 0; i < adapter.getCount(); i++) {
+            materialTabHost.addTab(
+                    materialTabHost.newTab()
+                            .setIcon(adapter.getIcon(i))
+                            .setTabListener(this));
+        }
     }
 
     @Override
@@ -29,10 +62,53 @@ public class TabLibraryActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab materialTab) {
+        viewPager.setCurrentItem(materialTab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab materialTab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab materialTab) {
+
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        private int[] icons = {R.drawable.ic_images, R.drawable.ic_videos, R.drawable.ic_files,
+                R.drawable.ic_images, R.drawable.ic_videos, R.drawable.ic_files,
+                R.drawable.ic_images, R.drawable.ic_videos, R.drawable.ic_files};
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public Fragment getItem(int position) {
+            return MainActivity.MyFragment.getInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return icons.length;
+        }
+
+        private Drawable getIcon(int position) {
+            //noinspection deprecation
+            return getResources().getDrawable(icons[position]);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getResources().getStringArray(R.array.tabs)[position];
+        }
     }
 }
